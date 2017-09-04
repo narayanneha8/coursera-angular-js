dcddd7c5a68555cc1a2e541b1f82aff3cb396308
nehanarayan <neha@coursera.com>
2017-09-04 00:57:55 -0400

    added if else

--- Module3.assignment/app.js
+++ Module3.assignment/app.js
@@ -9,22 +9,27 @@ MenuCategoriesController.$inject = ['menuService'];
 function MenuCategoriesController(menuService) {
   var menu = this;
   menu.item = "";
-  //menu.found = [];
-
-menu.foundItems = function(){
-  console.log("foundItem method envoked");
- var promise = menuService.getMenuCategories(menu.item);
- menu.found = menuService.getFound();
+  menu.found = [];
 
 
+  menu.foundItems = function() {
+    try {
+      var promise = menuService.getMenuCategories(menu.item);
+      // console.log("please handle error");
+      // menu.found = menuService.getFound();
+    } catch(error) {
+      console.log("Catch: MenuCategoriesController");
+      menu.error = error.message;
+      console.log("menu.error", error.message);
+    }
+  };
 
- };
- menu.removeItem = function(itemIndex){
-   menuService.remove(itemIndex);
+  menu.found = menuService.getFound();
+  menu.removeItem = function(itemIndex) {
+  menuService.remove(itemIndex);
  };
 
 }
-
 
 MenuSearchService.$inject = ['$http'];
 function MenuSearchService($http) {
@@ -32,42 +37,49 @@ function MenuSearchService($http) {
   var found = [];
 
   service.getMenuCategories = function (userEntry) {
-    var promise = $http({
-      method: "GET",
-      url: ("https://davids-restaurant.herokuapp.com/menu_items.json")
-    });
+    if (userEntry!= "") {
+      var promise = $http({
+        method: "GET",
+        url: ("https://davids-restaurant.herokuapp.com/menu_items.json")
+      });
 
-    promise.then(function (response) {
-      var menuArray = response.data.menu_items;
-      // console.log("until line 19 executed");
-      // console.log("menu.item is : ", userEntry);
-      // console.log("menuArray length: ", menuArray.length);
+      promise.then(function(response) {
+        var menuArray = response.data.menu_items;
 
-      for(var i = 0; i <menuArray.length; i++){
-        var name = menuArray[i].description;
-        //console.log("menuArray description: ", name.toUpperCase());
-        if(name.toLowerCase().indexOf(userEntry) != -1){
-          var list={
-            name: menuArray[i].name,
-            shortName: menuArray[i].short_name,
-            description: menuArray[i].description
-          };
-          //console.log("inside for and if statement", menuArray[i].name);
-          found.push(list);
-          // console.log("found array items:  ", found);
+        for (var i = 0; i <menuArray.length; i++) {
+          var name = menuArray[i].description;
+          if(name.toLowerCase().indexOf(userEntry) != -1) {
+            var list={
+              name: menuArray[i].name,
+              shortName: menuArray[i].short_name,
+              description: menuArray[i].description
+            };
+            found.push(list);
+          }
         }
-      }
-     })
-    .catch(function(error){
-      console.log("something went wrong");
-    });
+      })
+   .catch(function(error){
+     console.log("something went wrong");
+  });
+
+    } else {
+      console.log("Null entry");
+      throw new Error("Nothing Found.");
+      console.log("userEntry throwing Exception - null entry ");
+    }
+
   };
 
-  service.getFound = function(){
+  service.getFound = function() {
+    console.log("lenth=", found.length);
+    if (found.lenth == 0) {
+      console.log("getFound: length=0");
+      throw new Error("nothing found in the list");
+    }
     return found;
   };
 
-  service.remove = function(index){
+  service.remove = function(index) {
     found.splice(index,1);
   }
 }
